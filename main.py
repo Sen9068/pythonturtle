3f041d5a2c63c808a03c1c3e8074e7f8d7bb02c6
Sníček <102615758+Sen9068@users.noreply.github.com>
2025-03-11 17:15:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ turtle.bgcolor("black")
 t = turtle.Turtle()
 t.speed(10)
 turtle.colormode(255)
-colors = ["red", "blue"]
 
 def rgb_color(i):
     r = (i * 3) % 256
@@ -18,8 +17,7 @@ turtle.tracer(2, 10)
 t.hideturtle()
 for number in range(400):
     t.pencolor(rgb_color(number)) 
-    t.forward(number + 1)
-    t.pencolor(colors[number % 2])
+    t.forward(number + 10 * 2)
     t.right(89)
     t.pensize(0.2)
 
